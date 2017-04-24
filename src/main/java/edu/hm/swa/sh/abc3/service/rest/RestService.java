6dda20f213c581842ada0c5c8f33c2623f63ca28
Sebastian Hartl <sese.hartl@gmail.com>
2017-04-24 13:31:47 +0200

    current state

--- src/main/java/edu/hm/swa/sh/abc3/service/rest/RestService.java
+++ src/main/java/edu/hm/swa/sh/abc3/service/rest/RestService.java
@@ -9,10 +9,9 @@ import edu.hm.swa.sh.abc3.service.MediaServiceResult;
 import javax.inject.Inject;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
-import java.awt.*;
 
 /**
- * Created by Sebastian on 21.04.2017.
+ * REST Service outbound implementation.
  */
 @Path("media")
 @Consumes(MediaType.APPLICATION_JSON)
@@ -22,48 +21,97 @@ public class RestService {
     @Inject
     private MediaService mediaService;
 
+    /**
+     * Create a new book.
+     *
+     * @param book Book to be created.
+     * @return when a error occur: {@code MediaServiceResult.INVALID_ISBN}, {@code MediaServiceResult.ISBN_ALREADY_EXISTS},
+     * {@code MediaServiceResult.AUTHOR_MISSING}, {@code MediaServiceResult.TITLE_MISSING}
+     */
     @Path("books")
     @POST
     public MediaServiceResult addBook(final Book book) {
         return null;
     }
 
+    /**
+     * Returns the book matching the given ISBN.
+     *
+     * @return empty Medium-array if no book found or the book which matching the ISBN.
+     */
     @Path("books/{isbn}")
     @GET
     public Medium[] getBook() {
         return null;
     }
 
+    /**
+     * Returns all books.
+     *
+     * @return Medium-array with all books stored.
+     */
     @Path("books")
     @GET
     public Medium[] getBooks() {
         return null;
     }
 
+    /**
+     * Updates information about a book.
+     *
+     * @param book Book data to update.
+     * @return when an error occur: {@code MediaServiceResult.ISBN_IS_IMMUTABLE},
+     * {@code MediaServiceResult.INVALID_ISBN}, {@code MediaServiceResult.AUTHOR_MISSING},
+     * {@code MediaServiceResult.TITLE_MISSING}
+     */
     @Path("books/{isbn}")
     @PUT
     public MediaServiceResult updateBooks(final Book book) {
         return null;
     }
 
+    /**
+     * Returns the disk matching the given barcode.
+     *
+     * @return empty Medium-array if no disc found or the disc which matching the barcode.
+     */
     @Path("discs/{barcode}")
     @POST
     public Medium[] getDisc() {
         return null;
     }
 
+    /**
+     * Returns all stored discs.
+     *
+     * @return Medium-array with all books stored.
+     */
     @Path("discs")
     @POST
     public Medium[] getDiscs() {
         return new Medium[0];
     }
 
+    /**
+     * Create a new disc.
+     *
+     * @param disc Disc to be created.
+     * @return when a error occur: {@code MediaServiceResult.INVALID_BARCODE},
+     * {@code MediaServiceResult.BARCODE_ALREADY_EXISTS}, {@code MediaServiceResult.DIRECTOR_MISSING}
+     */
     @Path("discs")
     @POST
     public MediaServiceResult addDisc(final Disc disc) {
         return null;
     }
 
+    /**
+     * Updates information about a disc.
+     *
+     * @param disc disc data to update.
+     * @return when an error occur: {@code MediaServiceResult.BARCODE_IS_IMMUTABLE},
+     * {@code MediaServiceResult.INVALID_BARCODE}, {@code MediaServiceResult.DIRECTOR_MISSING}
+     */
     @Path("discs/{barcode}")
     @PUT
     public MediaServiceResult updateDisc(final Disc disc) {
