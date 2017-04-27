57fdd59d43830004766ec40db3c2b755fa2190e5
Sebastian <sese.hartl@gmail.com>
2017-04-27 13:18:14 +0200

    satisfy checkstyle

--- src/main/java/edu/hm/JettyStarter.java
+++ src/main/java/edu/hm/JettyStarter.java
@@ -1,27 +1,23 @@
 package edu.hm;
 
-import org.eclipse.jetty.server.*;
-import org.eclipse.jetty.servlet.ServletContextHandler;
-import org.eclipse.jetty.servlet.ServletHolder;
-import org.eclipse.jetty.webapp.*;
-import org.glassfish.jersey.server.ResourceConfig;
-import org.glassfish.jersey.servlet.ServletContainer;
-
-import javax.servlet.ServletContext;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.webapp.WebAppContext;
 
 /**
  * Start the application without an AppServer like tomcat.
- * @author <a mailto:axel.boettcher@hm.edu>Axel B&ouml;ttcher</a>
  *
+ * @author <a mailto:axel.boettcher@hm.edu>Axel B&ouml;ttcher</a>
  */
+// CHECKSTYLE:OFF
 public class JettyStarter {
 
-    public static final String APP_URL = "/";
-    public static final int PORT = 8082;
-    public static final String WEBAPP_DIR = "./src/main/webapp/";
+    private static final String APP_URL = "/";
+    private static final int PORT = 8082;
+    private static final String WEBAPP_DIR = "./src/main/webapp/";
 
     /**
      * Deploy local directories using Jetty without needing a container-based deployment.
+     *
      * @param args unused
      * @throws Exception might throw for several reasons.
      */
---
+++ src/main/java/edu/hm/swa/sh/abc3/rest/ServiceWorker.java
@@ -0,0 +1,7 @@
+package edu.hm.swa.sh.abc3.rest;
+
+/**
+ * Working class for REST service.
+ */
+public class ServiceWorker {
+}
